d8dfab33b11e314d8c3c14fb1749b512bc1945b6
Angélica Herrera <angieherc@gmail.com>
2023-08-02 15:55:14 +0200

    multiple changes

--- yoga/js/custom.js
+++ yoga/js/custom.js
@@ -334,3 +334,12 @@
 
 })();
 
+window.onscroll = function () {
+	var scrolled = window.pageYOffset || document.documentElement.scrollTop,
+		 topBtn = document.getElementById('topBtn');
+	   if (scrolled > 300 ) {
+		topBtn.classList.add('visible'); }
+		else { topBtn.classList.remove('visible');}
+   }
+  
+  
